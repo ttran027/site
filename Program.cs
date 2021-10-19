98cf8167d9e5100102c8a3d3419844cff9341e37
Thinh Tran <56406739+ttran027@users.noreply.github.com>
2021-10-19 11:02:38 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using MudBlazor.Services;
 
 namespace Website.Personal
 {
@@ -18,7 +19,8 @@ namespace Website.Personal
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+            builder.Services.AddMudServices();
+            
             await builder.Build().RunAsync();
         }
     }
