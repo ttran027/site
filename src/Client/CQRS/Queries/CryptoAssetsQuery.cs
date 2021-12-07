ca100115ad1006b910c3f562c4dd62abc6f158f4
Thinh Tran <tdtran04@gmail.com>
2021-12-06 23:16:11 -0500

    Query crypto assets from json

---
+++ src/Client/CQRS/Queries/CryptoAssetsQuery.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.Http.Json;
+
+namespace Client.CQRS.Queries
+{
+    public class CryptoAssetsQuery : IRequest<Result<IReadOnlyCollection<CryptoAsset>>>
+    {
+    }
+
+    public class CryptoAssetsQueryHandler : IRequestHandler<CryptoAssetsQuery, Result<IReadOnlyCollection<CryptoAsset>>>
+    {
+        private readonly HttpClient _httpClient;
+
+        public CryptoAssetsQueryHandler(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient("local");
+        }
+
+        public async Task<Result<IReadOnlyCollection<CryptoAsset>>> Handle(CryptoAssetsQuery request, CancellationToken cancellationToken)
+        {
+            return await Result.Try(async () =>
+            {
+                var response = await _httpClient.GetFromJsonAsync<IReadOnlyCollection<CryptoAsset>>("cryptos.json");
+                if (response == null)
+                {
+                    throw new Exception("Not Found");
+                }
+                return response;
+            },
+            ex => new Error("Failed to get crypto assets: " + ex.Message));
+        }
+    }
+
+    public record CryptoAsset
+    (
+        string Symbol,
+        string Name
+    );
+}
--- src/Client/Pages/CryptoPage.razor.cs
+++ src/Client/Pages/CryptoPage.razor.cs
@@ -5,14 +5,25 @@ namespace Client.Pages
 {
     public sealed partial class CryptoPage
     {
+        private List<CryptoInfo> _assets =  new List<CryptoInfo>();
+
         [Inject]
         private IMediator Mediator { get; set; } = null!;
 
-        private readonly List<string> Assets = new() { "0x-ZRX", "1inch-1INCH", "Aave-AAVE", "Alchemy Pay-ACH", "Adventure Gold-AGLD", "Algorand-ALGO", "Amp-AMP", "Ampleforth Governance Token-FORTH", "Ankr-ANKR", "Augur-REP", "Avalanche-AVAX", "Axie Infinity-AXS", "Balancer-BAL", "Bancor Network Token-BNT", "Band Protocol-BAND", "BarnBridge-BOND", "Basic Attention Token-BAT", "Bitcoin-BTC", "Bitcoin Cash-BCH", "Braintrust-BTRST", "Cardano-ADA", "Cartesi-CTSI", "Celo-CGLD", "Chainlink-LINK", "Chiliz-CHZ", "Civic-CVC", "Clover Finance-CLV", "Compound-COMP", "Cosmos-ATOM", "COTI-COTI", "Curve DAO Token-CRV", "Dai-DAI", "Dash-DASH", "Decentraland-MANA", "DerivaDAO-DDX", "DFI.Money-YFII", "District0x-DNT", "Dogecoin-DOGE", "Enjin Coin-ENJ", "Enzyme-MLN", "EOS-EOS", "Ethereum-ETH", "Ethereum Classic-ETC", "Fetch.ai-FET", "Filecoin-FIL", "Function X-FX", "Gitcoin-GTC", "Golem-GNT", "Harvest Finance-FARM", "Horizen-ZEN", "iExec RLC-RLC", "Internet Computer-ICP", "IoTeX-IOTX", "Jasmy-JASMY", "Keep Network-KEEP", "Kyber Network-KNC", "Litecoin-LTC", "Livepeer (LPT)-LPT", "Loom Network-LOOM", "Loopring-LRC", "Maker-MKR", "Mask Network-MASK", "Mirror Protocol-MIR", "NKN-NKN", "NuCypher-NU", "Numeraire-NMR", "OMG Network-OMG", "Orchid-OXT", "Origin Token-OGN", "Orion Protocol-ORN", "Paxos Standard-PAX", "PlayDapp-PLA", "Polkadot-DOT", "Polygon-MATIC", "Polymath-POLY", "Quant-QNT", "QuickSwap-QUICK", "Radicle-RAD", "Rai Reflex Index-RAI", "Rally-RLY", "Rari Governance Token-RGT", "Ren-REN", "Request-REQ", "Shiba Inu-SHIB", "SKALE-SKL", "Solana-SOL", "Stellar Lumens-XLM", "STORJ-STORJ", "SushiSwap-SUSHI", "Synthetix Network Token-SNX", "tBTC-TBTC", "Tellor-TRB", "TerraUSD-UST", "Tether-USDT", "Tezos-XTZ", "The Graph-GRT", "Tribe-TRIBE", "TrueFi-TRU", "UMA-UMA", "Uniswap-UNI", "USD Coin-USDC", "Wrapped Bitcoin-WBTC", "Wrapped Centrifuge-wCFG", "Wrapped LUNA-WLUNA", "XYO Network-XYO", "yearn.finance-YFI", "Zcash-ZEC" };
 
         private readonly string PanelStyle = "display: flex; flex-direction: column; align-items: center; width: 460px";
 
         private readonly string Base = "USDT";
+
+        protected async override Task OnInitializedAsync()
+        {
+            var result = await Mediator.Send(new CryptoAssetsQuery());
+            if (result.IsSuccess)
+            {
+                _assets = result.Value.Select(e => new CryptoInfo(e.Symbol, e.Name)).ToList();
+            }
+        }
+
         private static RenderFragment GetTitle(string titleText, string href) => builder =>
         {
             builder.OpenElement(0, "div");
@@ -36,14 +47,6 @@ namespace Client.Pages
             builder.CloseComponent();
             builder.CloseElement();
         };
-
-        private static CryptoInfo GetCryptoInfo(string c)
-        {
-            var terms = c.Split('-');
-            return new CryptoInfo(terms[1], terms[0]);
-        }
-
-        private List<CryptoInfo> SupportedCrypto => Assets.Select(e => GetCryptoInfo(e)).ToList();
 
         private async Task<CryptoPrice> GetPriceAsync(string symbol)
         {
