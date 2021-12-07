0d7a4f9d8526aae7f40130ca6760aec7e7f4c8fe
Thinh Tran <tdtran04@gmail.com>
2021-12-06 23:19:58 -0500

    Remove theme from DI container

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -23,31 +23,4 @@ builder.Services.AddHttpClient("binance", httpClient =>
     httpClient.BaseAddress = new Uri("https://api.binance.com/api/v3/");
 });
 builder.Services.AddMudServices();
-builder.Services.AddSingleton<MudTheme>(new MudTheme()
-    {
-        Palette = new Palette()
-        {
-            Black = "#27272f",
-            Background = "#202124",
-            BackgroundGrey = "#27272f",
-            Surface = "#373740",
-            DrawerBackground = "#27272f",
-            DrawerText = "rgba(255,255,255, 0.50)",
-            DrawerIcon = "rgba(255,255,255, 0.50)",
-            AppbarBackground = "#27272f",
-            AppbarText = "rgba(255,255,255, 0.70)",
-            TextPrimary = "#e8eaed",
-            TextSecondary = "#9aa0a6",
-            Secondary = "#9aa0a6",
-            ActionDefault = "#adadb1",
-            ActionDisabled = "rgba(255,255,255, 0.26)",
-            ActionDisabledBackground = "rgba(255,255,255, 0.12)",
-            Divider = "rgba(255,255,255, 0.12)",
-            DividerLight = "rgba(255,255,255, 0.06)",
-            TableLines = "rgba(255,255,255, 0.12)",
-            LinesDefault = "rgba(255,255,255, 0.12)",
-            LinesInputs = "rgba(255,255,255, 0.3)",
-            TextDisabled = "rgba(255,255,255, 0.2)"
-        }
-    });
 await builder.Build().RunAsync();
