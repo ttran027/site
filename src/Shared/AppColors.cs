8185e696ad39519287eec9951a686f4174d81037
Thinh Tran <tdtran04@gmail.com>
2021-12-09 00:05:21 -0500

    Change App colors

---
+++ src/Shared/AppColors.cs
@@ -0,0 +1,23 @@
+﻿namespace Shared
+{
+    public static class AppColors
+    {
+        public const string Blue50 = "#F0F4F8";
+        public const string Blue100 = "#D9E2EC";
+        public const string Blue200 = "#BCCCDC";
+        public const string Blue300 = "#9FB3C8";
+        public const string Blue400 = "#829AB1";
+        public const string Blue500 = "#627D98";
+        public const string Blue600 = "#486581";
+        public const string Blue700 = "#334E68";
+        public const string Blue800 = "#243B53";
+        public const string Blue900 = "#102A43";
+        public const string DarkGrey = "#121212";
+        public const string Primary = "#303F9F";
+        public const string Secondary = "#BDBDBD";
+        public const string Confirm = "#00897B";
+        public const string Warning = "#303F9F";
+        public const string Remove = "#C62828";
+        public const string Help = "#673AB7";
+    }
+}
