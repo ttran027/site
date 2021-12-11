c34322d900c5120b0f0d5b256c8ebb82cdef53b6
Thinh Tran <tdtran04@gmail.com>
2021-12-11 00:54:34 -0500

    Redesigned GainCalulator

--- src/Client/Common/Ultilities.cs
+++ src/Client/Common/Ultilities.cs
@@ -11,5 +11,16 @@
 
         public static DateTime EnsureStartOfMonth(this DateTime date)
             => new(date.Year, date.Month, 1);
+
+        public static RenderFragment ToPercent(this decimal p, Typo? typo) => builder =>
+        {
+            var price = p >= 0 ? $"+{p:N2}" : p.ToString("N2");
+            builder.OpenComponent<MudText>(0);
+            var color = p < 0 ? "red-text" : "green-text";
+            builder.AddAttribute(1, "Class", color);
+            builder.AddAttribute(2, "Typo", typo);
+            builder.AddAttribute(3,"ChildContent", (RenderFragment)(builder2 => { builder2.AddContent(4,price + "%"); }));
+            builder.CloseComponent();        
+        };
     }
 }
--- src/Client/Components/Crypto/GainCalculator/GainCalculatorOutput.cs
+++ src/Client/Components/Crypto/GainCalculator/GainCalculatorOutput.cs
@@ -2,7 +2,9 @@
 {
     public class GainCalculatorOutput
     {
+        public decimal Invesment { get; set; }
         public decimal Gain { get; set; }
         public decimal GainRate { get; set; }
+        public decimal GetFinal() => Gain + Invesment;
     }
 }
