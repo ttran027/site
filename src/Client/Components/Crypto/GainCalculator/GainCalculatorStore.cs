36b8fe5d62e36c6d0e2b10e1e746b30e00d8ec54
Thinh Tran <tdtran04@gmail.com>
2021-12-17 15:29:18 -0500

    Add state storage for GainCalculator

---
+++ src/Client/Components/Crypto/GainCalculator/GainCalculatorStore.cs
@@ -0,0 +1,35 @@
+﻿namespace Client.Components.Crypto.GainCalculator
+{
+    public record GainCalculatorState
+    (
+        GainCalculatorInput Input,
+        GainCalculatorOutput Output
+    );
+
+    public class GainCalculatorFeature : Feature<GainCalculatorState>
+    {
+        public override string GetName() => "GainCalculator";
+
+        protected override GainCalculatorState GetInitialState()
+         => new(
+                new GainCalculatorInput() { Amount = 1000 },
+                new GainCalculatorOutput());
+    }
+
+    public class GainCalculatorInputsEnterAction
+    {
+        public GainCalculatorInput Input { get; set; }
+        public GainCalculatorOutput Output { get; set; }
+    }
+
+    public static class GainCalculatorReducers
+    {
+        [ReducerMethod]
+        public static GainCalculatorState OnEnter(GainCalculatorState state, GainCalculatorInputsEnterAction action)
+            => state with
+            {
+                Input = action.Input,
+                Output = action.Output
+            };
+    }
+}
