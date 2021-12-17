4d2b36651b40eaee0c18bbf972921639c0684d41
Thinh Tran <tdtran04@gmail.com>
2021-12-17 15:10:16 -0500

    Add state storage for price search

---
+++ src/Client/Components/Crypto/PricesTable/PricesTableComponent.razor.cs
@@ -0,0 +1,48 @@
+﻿namespace Client.Components.Crypto.PricesTable
+{
+    public partial class PricesTableComponent
+    {
+        private string _searchString = string.Empty;
+
+        /// <summary>
+        /// A function that takes in symbol name as <see cref="string"/> and returns <seealso cref="CryptoPrice"/>.
+        /// This function must be passed to display the price.
+        /// </summary>
+        [Parameter]
+        [EditorRequired]
+        public Func<string, Task<CryptoPrice>> GetPrice { get; set; }
+
+        /// <summary>
+        /// A list of crypto assets to be supported int this table
+        /// </summary>
+        [Parameter]
+        public ICollection<CryptoInfo> Assets { get; set; }
+
+        [Inject]
+        private IState<SearchState> State { get; set; }
+
+        [Inject]
+        public IDispatcher Dispatcher { get; set; }
+
+        protected override void OnInitialized()
+        {
+            _searchString = State.Value.SearchTerm;
+            base.OnInitialized();
+        }
+
+        private void UpdateSearchState()
+        {
+            Dispatcher.Dispatch(new SearchEnterAction { Term = _searchString });
+        }
+
+        private bool Search(CryptoInfo info)
+        {           
+            if (!string.IsNullOrEmpty(_searchString))
+            {
+                return info.Name.Contains(_searchString, StringComparison.OrdinalIgnoreCase) ||
+                        info.Symbol.Contains(_searchString, StringComparison.OrdinalIgnoreCase);
+            }
+            return true;
+        }
+    }
+}
---
+++ src/Client/Components/Crypto/PricesTable/SearchStore.cs
@@ -0,0 +1,35 @@
+﻿namespace Client.Components.Crypto.PricesTable
+{ 
+    public record SearchState
+    (
+        string SearchTerm
+    );
+
+    
+    public class SearchFeature : Feature<SearchState>
+    {
+        public override string GetName() => "Search";
+
+        protected override SearchState GetInitialState()
+        {
+            return new SearchState(string.Empty);
+        }
+    }
+
+    public class SearchEnterAction
+    {
+        public string Term { get; set; }
+    }
+
+    public static class SearchReducers
+    {
+        [ReducerMethod]
+        public static SearchState OnSearch(SearchState state, SearchEnterAction action)
+        {
+            return state with
+            {
+                SearchTerm = action.Term,
+            };
+        }
+    }
+}
